10fb397593ffdb50b33008f516c1363da58c4073
DairoCamacho <74800666+DairoCamacho@users.noreply.github.com>
2024-04-05 19:08:07 -0500

    se agrega cifrado al password al momento de crear una entidad cliente

--- src/main/java/com/unaux/dairo/api/domain/client/Client.java
+++ src/main/java/com/unaux/dairo/api/domain/client/Client.java
@@ -2,9 +2,9 @@ package com.unaux.dairo.api.domain.client;
 
 import com.unaux.dairo.api.domain.user.User;
 import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDate;
+import lombok.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Getter
 @Setter
@@ -12,45 +12,50 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @ToString
 @Entity
-@Table(name="client")
+@Table(name = "client")
 @PrimaryKeyJoinColumn
 public class Client extends User {
-    private String type;
 
+  private String type;
 
-    public Client(ClientCreateDto clientCreateDto) {
-        setStatus(true);
-        setName(clientCreateDto.name());
-        setLastName(clientCreateDto.lastName());
-        setPhone(clientCreateDto.phone());
-        setBirthday(LocalDate.parse(clientCreateDto.birthday()));
-        setEmail(clientCreateDto.email());
-        setPassword(clientCreateDto.password());
-        setType("new");// por defecto todos serán nuevos
+  public Client(ClientCreateDto clientCreateDto) {
+    setStatus(true);
+    setName(clientCreateDto.name());
+    setLastName(clientCreateDto.lastName());
+    setPhone(clientCreateDto.phone());
+    setBirthday(LocalDate.parse(clientCreateDto.birthday()));
+    setEmail(clientCreateDto.email());
+    setPassword(encryptPassword(clientCreateDto.password()));
+    setType("new"); // por defecto todos serán nuevos
+  }
+
+  public void update(ClientUpdateDto clientUpdateDto) {
+    if (clientUpdateDto.name() != null) {
+      setName(clientUpdateDto.name());
     }
-
-    public void update(ClientUpdateDto clientUpdateDto) {
-        if (clientUpdateDto.name() != null) {
-            setName(clientUpdateDto.name());
-        }
-        if (clientUpdateDto.lastName() != null) {
-            setLastName(clientUpdateDto.lastName());
-        }
-        if (clientUpdateDto.phone() != null) {
-            setPhone(clientUpdateDto.phone());
-        }
-        if (clientUpdateDto.birthday() != null) {
-            setBirthday(LocalDate.parse(clientUpdateDto.birthday()));
-        }
-        if (clientUpdateDto.type() != null) {
-            setType(clientUpdateDto.type());
-        }
-        if (clientUpdateDto.email() != null) {
-            setEmail(clientUpdateDto.email());
-        }
+    if (clientUpdateDto.lastName() != null) {
+      setLastName(clientUpdateDto.lastName());
     }
-
-    public void inactivate() {
-        setStatus(false);
+    if (clientUpdateDto.phone() != null) {
+      setPhone(clientUpdateDto.phone());
     }
+    if (clientUpdateDto.birthday() != null) {
+      setBirthday(LocalDate.parse(clientUpdateDto.birthday()));
+    }
+    if (clientUpdateDto.type() != null) {
+      setType(clientUpdateDto.type());
+    }
+    if (clientUpdateDto.email() != null) {
+      setEmail(clientUpdateDto.email());
+    }
+  }
+
+  public void inactivate() {
+    setStatus(false);
+  }
+
+  public String encryptPassword(String password) {
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    return encoder.encode(password);
+  }
 }
