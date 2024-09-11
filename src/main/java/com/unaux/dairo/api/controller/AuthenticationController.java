41da5e6679625d2d0a810dd4011b0b214a09932e
DairoCamacho <74800666+DairoCamacho@users.noreply.github.com>
2024-09-10 19:27:04 -0500

    feat: Agregar documentación Swagger al controlador de autenticación
    Agrega anotaciones de Swagger al controlador de autenticación para documentar la API y generar la especificación Swagger.

--- src/main/java/com/unaux/dairo/api/controller/AuthenticationController.java
+++ src/main/java/com/unaux/dairo/api/controller/AuthenticationController.java
@@ -14,11 +14,17 @@ import com.unaux.dairo.api.domain.user.UserAuthenticationDto;
 import com.unaux.dairo.api.infra.security.JwtTokenDto;
 import com.unaux.dairo.api.infra.security.TokenService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 // Esta clase es un controlador REST que maneja las solicitudes relacionadas con la autenticación de usuarios.
 @RestController
 @RequestMapping("api/v1/login")
+@Tag(name = "Authentication", description = "Controller for Authentication")
 public class AuthenticationController {
 
   // para disparar el proceso de autenticación en spring existe la clase
@@ -37,6 +43,27 @@ public class AuthenticationController {
   // Este método toma un objeto UserAuthenticationDto como parámetro, que
   // probablemente contiene las credenciales del usuario (email y password)
   @PostMapping
+  @Operation(
+    summary="Login User",
+    description="Authenticate a user and return the JWT token",
+    tags={"Authentication"},
+    requestBody=@io.swagger.v3.oas.annotations.parameters.RequestBody(
+      description="User credentials",
+      required=true,
+      content=@Content(
+        mediaType="application/json",
+        schema=@Schema(implementation = UserAuthenticationDto.class)
+      )
+    ),
+    responses= @ApiResponse(
+      responseCode = "200", 
+      description = "User authenticated and JWT token generated", 
+      content = @Content(
+        mediaType = "application/json", 
+        schema = @Schema(implementation = JwtTokenDto.class)
+      )
+    )
+  )
   public ResponseEntity<JwtTokenDto> authenticateUser(
       @RequestBody @Valid UserAuthenticationDto userAuthenticationDto) {
     // creamos un token con las credenciales del usuario
