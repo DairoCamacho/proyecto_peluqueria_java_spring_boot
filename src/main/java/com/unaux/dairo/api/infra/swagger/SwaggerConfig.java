7a5fdf75654b896b1a0a0d38c8398ec5dd4d7e94
DairoCamacho <74800666+DairoCamacho@users.noreply.github.com>
2024-09-10 19:25:04 -0500

    refactor: Modificar configuración de Swagger usando anotaciones en lugar de Beans

--- src/main/java/com/unaux/dairo/api/infra/swagger/SwaggerConfig.java
+++ src/main/java/com/unaux/dairo/api/infra/swagger/SwaggerConfig.java
@@ -1,58 +1,42 @@
 package com.unaux.dairo.api.infra.swagger;
 
 import org.springframework.http.HttpHeaders;
-import java.util.List;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.security.SecurityScheme.In;
-import io.swagger.v3.oas.models.security.SecurityScheme.Type;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
-@Configuration
+@OpenAPIDefinition(info = @Info(
+                            title = "API RESTful for Hair Salon", 
+                            description = "API RESTful for Hair Salon", 
+                            version = "0.4.1", 
+                            termsOfService = "https://www.unaux.com/terms", 
+                            contact = @Contact(name = "Dairo API Support", 
+                              email = "dalecc08@outlook.com"),
+                            license = @License(
+                              name = "Apache 2.0", 
+                              url = "https://www.apache.org/licenses/LICENSE-2.0")  
+                              ),
+                          servers ={
+                            @Server(url = "http://localhost:8080", 
+                                    description = "Development Server"),
+                            @Server(url = "https://dairo-api.unaux.com", 
+                                    description = "Production Server")
+                          },
+                          security = @SecurityRequirement(name = "Security Token")
+                  )
+@SecurityScheme(type = SecuritySchemeType.HTTP, 
+                name = "Security Token", 
+                description = "JWT Token",
+                paramName = HttpHeaders.AUTHORIZATION,
+                in = SecuritySchemeIn.HEADER,
+                scheme = "bearer", 
+                bearerFormat = "JWT")
 public class SwaggerConfig {
-
-  @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("API RESTful for Hair Salon")
-                        .version("0.4.1")
-                        .description("API RESTful for Hair Salon")
-                        .termsOfService("https://www.unaux.com/terms")
-                        .contact(new Contact()
-                                .name("Dairo API Support")
-                                .email("dalecc08@outlook.com"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
-                .servers(List.of(
-                        new Server().url("http://localhost:8080").description("Development Server"),
-                        new Server().url("https://dairo-api.unaux.com").description("Production Server")
-                ))
-                .addSecurityItem(new SecurityRequirement().addList("Security Token"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("Security Token", new SecurityScheme()
-                                .type(Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                                .in(In.HEADER)
-                                .name(HttpHeaders.AUTHORIZATION)
-                                .description("JWT Token")));
-    }
-
-  @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("private")
-                .pathsToMatch("/api/**")
-                .build();
-    }
 
 }
